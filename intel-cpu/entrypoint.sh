8b760c57137a85119dd244e253ef1907b90fbbf9
temach <tematibr@gmail.com>
2025-02-06 20:28:30 +0500

    add docker compose for intel cpu

---
+++ intel-cpu/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start Ollama in the background.
+/bin/ollama serve &
+# Record Process ID.
+pid=$!
+
+# Pause for Ollama to start.
+sleep 5
+
+echo "🔴 Retrieve deepseek-r1:8b model..."
+# ollama pull deepseek-r1:8b
+# ollama pull deepseek-r1:7b
+echo "🟢 Done!"
+
+# Wait for Ollama process to finish.
+wait $pid
