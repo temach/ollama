6ebdef6ab397d65d40a60b218ae5fb625ddf61a4
temach <tematibr@gmail.com>
2026-02-17 08:55:25 +0500

    expose ollama container port 11434 so other tools can use it

--- intel-altgpu/entrypoint.sh
+++ intel-altgpu/entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# init ollama first
-mkdir -p /llm/ollama
-cd /llm/ollama
-init-ollama
-export OLLAMA_NUM_GPU=999
-export ZES_ENABLE_SYSMAN=1
-export OLLAMA_DEBUG=1
-export OLLAMA_HOST="0.0.0.0:11434"
-
-# Start Ollama in the background.
-./ollama serve &
-# Record Process ID.
-pid=$!
-
-# Pause for Ollama to start.
-sleep 5
-
-echo "🔴 Retrieve models..."
-./ollama pull deepseek-r1:8b
-# ./ollama pull deepseek-r1:7b
-# ./ollama pull deepseek-r1:1.5b
-echo "🟢 Done!"
-
-# Wait for Ollama process to finish.
-wait $pid
+# # init ollama first
+# mkdir -p /llm/ollama
+# cd /llm/ollama
+# init-ollama
+# export OLLAMA_NUM_GPU=999
+# export ZES_ENABLE_SYSMAN=1
+# export OLLAMA_DEBUG=1
+# export OLLAMA_HOST="0.0.0.0:11434"
+# 
+# # Start Ollama in the background.
+# ./ollama serve &
+# # Record Process ID.
+# pid=$!
+# 
+# # Pause for Ollama to start.
+# sleep 5
+# 
+# echo "🔴 Retrieve models..."
+# ./ollama pull deepseek-r1:8b
+# # ./ollama pull deepseek-r1:7b
+# # ./ollama pull deepseek-r1:1.5b
+# echo "🟢 Done!"
+# 
+# # Wait for Ollama process to finish.
+# wait $pid
